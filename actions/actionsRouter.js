db19f9bd8e36f89438ee17cd462da93446eaebb7
wcolts2000 <wcolts2000@gmail.com>
2019-01-11 09:15:46 -0800

    added get functionality and tested 3 endpoints on postman

---
+++ actions/actionsRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const projectsDb = require("../data/helpers/projectModel");
+
+const router = express.Router();
+
+// Get Single Project
+router.get("/:projectId", (req, res) => {
+  const { projectId } = req.params;
+  projectsDb
+    .get(projectId)
+    .then(project => {
+      if (project) {
+        res.status(200).json(project);
+      } else {
+        res
+          .status(404)
+          .json({ message: "a project with that ID does not exist" });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "the project info could not be retrieved" })
+    );
+});
+
+//  Get Single Projects Actions
+router.get("/:projectsId/actions", (req, res) => {
+  const { projectsId } = req.params;
+  projectsDb
+    .getProjectActions(projectsId)
+    .then(project => {
+      if (project.length) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({
+          message:
+            "a project with that id doesnt exist or does not have any actions with it"
+        });
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "the project info could not be retrieved" })
+    );
+});
+
+// Get Projects List
+router.get("/", (req, res) => {
+  projectsDb
+    .get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "the projects info could not be retrieved" })
+    );
+});
--- api/server.js
+++ api/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const configureMiddleware = require("../config/middleware.js");
 
 const projectsRouter = require("../projects/projectsRouter");
-const actionsRouter = require("../actions/actionsRouter");
+// const actionsRouter = require("../actions/actionsRouter");
 
 // Router Setup
 const router = express.Router();
 
 router.use("/api/projects", projectsRouter);
-router.use("/api/actions", actionsRouter);
+// router.use("/api/actions", actionsRouter);
 
 // Server Init
 const server = express();
@@ -19,6 +19,6 @@ configureMiddleware(server);
 // Routes
 
 server.use("/api/projects", projectsRouter);
-server.use("/api/actions", actionsRouter);
+// server.use("/api/actions", actionsRouter);
 
-module.exports = { server, router };
+module.exports = server;
